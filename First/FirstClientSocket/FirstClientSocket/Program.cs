3ae80156842d1d1aba684a1c03f4d2f27d639fc1
NeoTRAN <neo_tran@outlook.com>
2019-05-01 20:47:02 -0500

    First Commit

---
+++ First/FirstClientSocket/FirstClientSocket/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace FirstClientSocket
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            String ipAddress = "127.0.0.1";
+            int port = 8090;
+
+            Socket listen = new Socket(
+                  AddressFamily.InterNetwork,
+                  SocketType.Stream,
+                  ProtocolType.Tcp
+            );
+
+            IPEndPoint connect = new IPEndPoint(IPAddress.Parse(ipAddress), port);
+
+            listen.Connect(connect);
+
+            byte[] sends = new byte[100];
+            String data;
+
+            Console.WriteLine("Write a Data: ");
+            data = Console.ReadLine();
+            sends = Encoding.Default.GetBytes(data);
+
+            listen.Send(sends);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ First/FirstServerSocket/FirstServerSocket/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace FirstServerSocket
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            String ipAddress = "127.0.0.1";
+            int port = 8090;
+
+            Socket listen = new Socket(
+                  AddressFamily.InterNetwork,
+                  SocketType.Stream,
+                  ProtocolType.Tcp
+            );
+
+            Socket connection;
+            IPEndPoint connect = new IPEndPoint(IPAddress.Parse(ipAddress), port);
+
+            listen.Bind(connect);
+            listen.Listen(5);
+
+            connection = listen.Accept();
+            Console.WriteLine("Successful connection");
+
+            Byte[] receive = new Byte[100];
+            String data = "";
+            int arraySize = 0;
+
+            arraySize = connection.Receive(receive, 0, receive.Length, 0);
+
+            Array.Resize(ref receive, arraySize);
+            data = Encoding.Default.GetString(receive);
+
+            Console.WriteLine("Data: " + data);
+            Console.ReadKey();
+        }
+    }
+}
